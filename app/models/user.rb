7e8421e4d8526aae7d4d85caaebb3a38958e891f
kazu <skaz0110@yahoo.co.jp>
2021-10-16 17:36:43 +0900

    アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 end
