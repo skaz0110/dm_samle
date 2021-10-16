efa265487514866ea455fd220e14caffaf83d60b
kazu <skaz0110@yahoo.co.jp>
2021-10-16 17:40:54 +0900

    View

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, :only => [:index, :show]
+  root "users#index"
+  resources :messages, :only => [:create]
+  resources :rooms, :only => [:create, :show, :index]
 end
